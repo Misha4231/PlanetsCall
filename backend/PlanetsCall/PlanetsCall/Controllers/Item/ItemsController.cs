4a4b113e749d0e9349e5df4bf6fbc426bfab758a
Misha <mishagalko1@gmail.com>
2024-11-25 22:59:49 +0100

    create items controller

---
+++ backend/PlanetsCall/PlanetsCall/Controllers/Item/ItemsController.cs
@@ -0,0 +1,20 @@
+﻿using Data.Repository.Log;
+using Data.Repository.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlanetsCall.Controllers.Item;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ItemsController
+{
+    private readonly IUsersRepository _usersRepository;
+    private readonly ILogsRepository _logsRepository;
+    
+    public ItemsController(IUsersRepository usersRepository, ILogsRepository logsRepository)
+    {
+        _usersRepository = usersRepository;
+        _logsRepository = logsRepository;
+    }
+
+}
