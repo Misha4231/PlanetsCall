54c47f4e13f2650ceb7f53ab5b45e611310daa8f
Misha <mishagalko1@gmail.com>
2025-03-02 22:24:13 +0100

    fix database import

--- backend/PlanetsCall/entrypoint.sh
+++ backend/PlanetsCall/entrypoint.sh
@@ -6,7 +6,7 @@ until pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
   sleep 2
 done
 
-SQL_FILE="/tmp/world.sql"
+SQL_FILE="/data/world.sql"
 SQL_URL="https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/refs/heads/master/psql/world.sql"
 
 if [ -f "$SQL_FILE" ];
@@ -19,18 +19,25 @@ curl -o "$SQL_FILE" "$SQL_URL"
 fi
 
 # Check if the 'cities' table already contains data
-echo "Checking if 'cities' table has data..."
-TABLE_DATA=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -t -c "SELECT COUNT(*) FROM cities;")
+TABLE_EXISTS=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -t -c "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'cities');")
 
-if [ "$TABLE_DATA" -gt 0 ]; then
-  echo "The 'cities' table already contains data. Skipping import."
-else
-  echo "The 'cities' table is empty. Importing data..."
-  
+if [ !$TABLE_EXISTS ]; then
+  echo "'cities' table exists."
   # Import the SQL file into the database
   PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "$SQL_FILE"
-
   echo "Database imported successfully!"
+else
+  echo "Checking if 'cities' table has data..."
+  TABLE_DATA=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -t -c "SELECT COUNT(*) FROM cities;")
+
+  if [ "$TABLE_DATA" -gt 0 ]; then
+    echo "The 'cities' table already contains data. Skipping import."
+  else
+    echo "The 'cities' table is empty. Importing data..."
+    # Import the SQL file into the database
+    PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f "$SQL_FILE"
+    echo "Database imported successfully!"
+  fi
 fi
 
 # Start API
